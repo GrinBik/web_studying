2bb1b3a8536df0363bb59e9efb78cca29f6807cb
GrinBik <gregorybik@gmail.com>
2022-11-14 16:21:52 +0400

    codestyle error checked

--- main_page/views.py
+++ main_page/views.py
@@ -5,5 +5,6 @@ from django.http import HttpResponse
 def greeting_view(request):
     return render(request, 'main_page/greeting.html')
 
+
 def biography_view(request):
     return render(request, 'main_page/biography.html')
