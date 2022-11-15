2e9b7fcac25d576ecba572821f7271af45e11b6d
GrinBik <gregorybik@gmail.com>
2022-11-15 18:42:16 +0400

    create file and folder list of project

--- main_page/views.py
+++ main_page/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import glob
+import os
+ 
+
+def project_files(dir: str) -> list:
+    files = []
+    folders = []
+    for file in glob.glob(f'{dir}/*', recursive=True):
+        if os.path.isdir(file):
+            folders.append(file.strip(".\\"))
+        else:
+            files.append(file.strip(".\\"))
+    return files, folders
+
+
+def previous_page(dir) -> str:
+    if dir == ".":
+        return "main_page"
+    path = dir.split("\\")
+    if len(path) > 1:
+        path.pop()
+        file_path = "\\".join(path)
+        return file_path
+    else:
+        return "."
 
 
 def greeting_view(request):
@@ -8,3 +33,11 @@ def greeting_view(request):
 
 def biography_view(request):
     return render(request, 'main_page/biography.html')
+
+
+def project_view(request):
+    file_path = request.GET["dir"]
+    files, folders = project_files(file_path)
+    previous_path = previous_page(file_path)
+    content = {"files" : files, "folders" : folders, "previous_page" : previous_path}
+    return render(request, 'main_page/project.html', content)
